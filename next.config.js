4231ac0115d3e7d13770882eaae0726ee9f3cb0e
joyahmed <joy_ahmed_007@yahoo.com>
2022-03-14 21:35:44 +0600

    project init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const path = require('path');
 
-module.exports = nextConfig
+module.exports = {
+	reactStrictMode: true,
+	webpack: config => {
+		config.resolve.alias['@'] = path.resolve(__dirname);
+		return config;
+	},
+	images: {
+		domains: [
+			'res.cloudinary.com',
+			'a0.muscache.com',
+			'mdbcdn.b-cdn.net',
+			'unsplash.com',
+			'images.unsplash.com',
+			'lottiefiles.com',
+			'assets10.lottiefiles.com'
+		]
+	},
+	env: {}
+};
